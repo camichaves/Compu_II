85dcda34123f1e2a821faba96d7c65a25da4ce47
CAMILA-PC\CAMILA <camichavesthg@gmail.com>
2018-09-25 00:15:45 -0300

    Tp3 resuelto

--- TP3 -resuelto-/trabajo3.c
+++ TP3 -resuelto-/trabajo3.c
@@ -1,21 +1,27 @@
-	#include <sys/types.h>
-        #include <sys/stat.h>
-        #include <fcntl.h>
-	#include <sys/mman.h>
-	#include <unistd.h>
-	#include <sys/wait.h>
-	#include <semaphore.h>
-        #include <sys/wait.h>
-        #include <ctype.h>
-        #include <stdio.h>
-        #include <stdlib.h>
-        #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/mman.h>
+#include <semaphore.h>
+
 
 
 int Contador[21];
 const char* PalabrasReservadas[3]; //cuantas??
 char buff[1500];
 char buffauxiliar[1500];
+
 
 //funcion cererar contador
 void CerearContador(){
@@ -27,10 +33,9 @@ void CerearContador(){
 
 //Funcion lista palabras reservadas
 void Reservadas(){
-PalabrasReservadas[0]="ESTADO";
-PalabrasReservadas[1]="GOBIERNO";
-PalabrasReservadas[2]="CIUDAD";
-
+        PalabrasReservadas[0]="THESE";
+        PalabrasReservadas[1]="ADVENTURE";
+        PalabrasReservadas[2]="WITHOUT";
 }
 
 
@@ -95,92 +100,82 @@ int Resto(char* palabra, int indpal, int tam2){
         return rta;
 }
 
-
-
-
-
 //Funcion hijo 1 contador
-int Hijo1(int leido, char ** ptr){
+int Hijo1(int *leido, char *ptr , sem_t *sema, sem_t *sem2){
 
- 
-
-        char buf[220];
-        int tamanio;
+        
+  int tamanio;
         char * lineas[256];
         char d2[]="|123456789 - 0'¿°!«#$%&/()=—?¡[]_:;,.-{+}´ \n";
 
         int j;
+        CerearContador();        
+        while(*leido>0){
+                for(int i= 0; i<*leido;i++){
+		        buff[i]=ptr[i];
+	        }
+                j=0;
+                lineas[j]=strtok(buff,d2);
+                while(lineas[j]!= NULL){
+                        tamanio= strlen(lineas[j]);
+                        Contador[tamanio-1]++;
+                        j++;
+                        lineas[j]=strtok(NULL,d2);
 
-
-                        CerearContador();
-
-while((leido=read(*ptr, buf, sizeof buf))>0){
-
-        j=0;
-        lineas[j]=strtok(buf,d2);
-        while(lineas[j]!= NULL){
-                tamanio= strlen(lineas[j]);
-
-                Contador[tamanio-1]++;
-                j++;
-                lineas[j]=strtok(NULL,d2);
-
+                }
+                sem_post(sem2);
+                sem_wait(sema);
         }
-}
-        
-
-                // Contadores listos
+        // Contadores listos
         for(int g=0;g<21;g++){
         printf("\n Cantidad de palabras de %d caracteres:%d",g+1,Contador[g]);
             }
         printf("\n");
 
+        sem_post(sem2);
+      
+
         return 0;
-
-
 }
 
 
 
 //Funcion Hijo 2 conversor a mayus
-int Hijo2(int leido, char ** ptr){
-        
+int Hijo2(int leido, char *buff){
 
         Reservadas();
         int ban=0;
-        char d2[]="|123456789 - 0'¿°!«#$%&/()=—?¡[]_:;,.-{+}´ \n";
+        char d2[]="|123456789 - 0'¿°!«#$%&/()=—?¡[]_:;,.-{+}´ \n\t";
         char * lineas[256];
         int j,k=0;
         int indpal;
         int h=0;
         int tambuff;
-        while((leido=read(*ptr,buff, sizeof buff))>0){
-                tambuff=leido;
-                strncpy(buffauxiliar,buff,leido);
-
-
-                j=0;
-                lineas[j]=strtok(buff,d2);
-                while(lineas[j]!=NULL){
-                        indpal=Buscar(lineas[j]);
-                        h++;
-                        if(indpal!=-1){
-                                tambuff=Resto(lineas[j],indpal,tambuff);//imprime all hasta pal
-                                                j++;
-                        }else {
-
-                                j++;
-                        }
-
-                        lineas[j]=strtok(NULL,d2);
+     
+               
+        tambuff=leido;
+        strncpy(buffauxiliar,buff,leido);
+        j=0;
+        lineas[j]=strtok(buff,d2);
+        while(lineas[j]!=NULL){
+                indpal=Buscar(lineas[j]);
+                h++;
+                if(indpal!=-1){
+                        tambuff=Resto(lineas[j],indpal,tambuff);//imprime all hasta pal
+                        j++;
+                }else {
+                        j++;
                 }
+
+                lineas[j]=strtok(NULL,d2);
+        }
 
                 printf("%s",buffauxiliar);
 
                 memset(buffauxiliar,'\0',tambuff);
-        }
-
         
+
+       
         return 0;
 
 }
@@ -191,31 +186,26 @@ int Hijo2(int leido, char ** ptr){
 
 int main(int argc, char * const argv[]){
 
-	sem_t *semhijo1, *sem1hijo1, *sem2hijo2, *semhijo2;
-	char* ptr;
-
-	semhijo1=(sem_t *)ptr+ sizeof semhijo1; //semaforos en  memcomp
-	semhijo2=(sem_t *)ptr+ (sizeof semhijo1)*2;
-	sem1hijo1= (sem_t *)ptr+ (sizeof semhijo1)*3;
-	sem2hijo2= (sem_t *)ptr+ (sizeof semhijo1)*4;
-	sem_init(semhijo1,1,0);	
-	sem_init(semhijo2,1,0);
-	sem_init(sem1hijo1,1,0);
-        sem_init(sem2hijo2,1,0);
-
-	sem_wait(semhijo1);
-	sem_wait(semhijo2);
-
-	ptr=mmap(NULL,10,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_SHARED,-1,0);
-
-
-        int leido;
         int flagi=0;
         char buffer[2200];
         char *program_name;
         char* opcion_i = NULL;
         int c;
-        
+        char *ptr;
+        ptr = mmap(NULL,10,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_SHARED,-1, 0);
+        int *leido;
+        //leido = mmap(NULL,10,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_SHARED,-1, 0);
+        sem_t *sema, *sem2, *sema1, *sema2;
+       
+	sema = (sem_t *) ptr + sizeof sema;
+	sem2 = sema + sizeof sem2;
+	sema1= sem2 + sizeof sema1;
+	sema2= sema1 + sizeof sema2;
+        leido= (int *)sema2+ sizeof leido;
+	sem_init(sema,1,0);
+	sem_init(sem2,1,0);
+	sem_init(sema1,1,0);
+	sem_init(sema2,1,0);
 
 
         /* bucle para loopear por las opciones pasadas */
@@ -231,7 +221,6 @@ int main(int argc, char * const argv[]){
         }
                 }
 
-       
 
     int procnum;
     int pid;
@@ -243,39 +232,45 @@ int main(int argc, char * const argv[]){
     }
 
 if(procnum==1){
-sem_wait(semhijo1);
-Hijo1(leido,*ptr);
-sem_post(sem1hijo1);
-return 0;
+        sem_wait(sema); //sem rojo
+        Hijo1(leido, ptr,sema,sem2);          
+        return 0;
 }
 
 if(procnum==2){
-sem_wait(semhijo2);
-Hijo2(leido,*ptr);
-sem_post(sem2hijo2);
+        do{
+        sem_wait(sema1);
+         for(int i= 0; i<*leido;i++){
+		buff[i]=ptr[i];
+	}
+        Hijo2(*leido, buff);
+        sem_post(sema2);
+        }while(*leido!=0); 
 return 0;
 }
 
-       
 
         if(flagi!=0){
                 int arch= open(opcion_i, O_RDWR , 0666);
 
-                while((leido=read(arch,&buffer,sizeof buffer))>0){
-                        write(*ptr,buffer,leido);
-			sem_post(semhijo1);
-			sem_post(semhijo2);
-			sem_wait(sem1hijo1);
-			sem_wait(sem2hijo2);
-                        
+               while((*leido=read(arch,ptr,120))>0){
+                     
+                        //printf("-PADRE: ENTRO WHILE \n");
+                       //write(STDOUT_FILENO,ptr,*leido);
+                        sem_post(sema1);
+			sem_post(sema);
+
+			sem_wait(sema2);
+                       sem_wait(sem2);
+                        //write
+                        //write
                 }
+                sem_post(sema);
+                 close(arch);
         }else{
-                while((leido=read(STDIN_FILENO,buffer,sizeof buffer))>0){
-			write(*ptr,buffer, leido);
-			sem_post(semhijo1);
-                        sem_post(semhijo2);
-                        sem_wait(sem1hijo1);
-                        sem_wait(sem2hijo2);
+                while((*leido=read(STDIN_FILENO,buffer,sizeof buffer))>0){
+                         // write
+                         //write
 
                 }
         }
